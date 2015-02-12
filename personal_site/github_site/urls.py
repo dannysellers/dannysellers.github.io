5662aea71e6a83455cd350aa0558965925e8354e
danny <dsellers90@gmail.com>
2015-02-11 23:11:49 -0800

    initial commit of single page site.

--- personal_site/github_site/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from django.conf import settings
-from django.conf.urls.static import static
-
-import views
-
-urlpatterns = patterns('',
-	url(r'^$', views.home, name='index'),
-)
--- personal_site/github_site/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-
-def home (request):
-	context = RequestContext(request)
-	context_dict = {}
-
-	return render_to_response('github_site/index.html', context_dict, context)
